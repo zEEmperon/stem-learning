aa46410d88593123229fcd7ab802365bdc9dbe16
zEEmperon <dan4ik2204.danya@gmail.com>
2023-03-11 21:01:41 +0200

    finished out screen moving animation for chalkboard

--- unity-ui/Assets/PlaceObjectsOutsideScreen.cs
+++ unity-ui/Assets/PlaceObjectsOutsideScreen.cs
@@ -7,19 +7,10 @@ public class PlaceObjectsOutsideScreen : MonoBehaviour
 
     void Start()
     {
-        Camera camera = Camera.main;
-        
-        float screenWidth = Screen.width;
-        
-        Vector3 rightEdge = camera.ScreenToWorldPoint(new Vector3(screenWidth, 0, camera.nearClipPlane));
-        
-
-
         foreach (var o in objectsToPlace)
         {
-            float xOffset = o.transform.localScale.x / 2.0f; 
             Vector3 newPosition = o.transform.position;
-            newPosition.x = rightEdge.x + xOffset;
+            newPosition.x = 50;
             o.transform.position = newPosition;
         }
     }
